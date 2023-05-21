a450468512ef1af6d716a65e6d986bb74d76f9f6
shiüo <shiueo.csh@gmail.com>
2023-05-21 17:37:29 +0900

    comments added

--- tools/build.py
+++ tools/build.py
@@ -41,31 +41,48 @@ def build(
     include_package_dict,
 ):
     try:
+        # Formatting the code and creating requirements.txt file
         clear.code_format_and_make_requirements_txt(path=os.path.dirname(path))
+
+        # Define build file name and output directory name
         buildfile_name = path
         Output_dir_name = os.path.join(
             os.path.dirname(path), f"{pathlib.Path(path).stem}_build"
         )
 
+        # Creating a list to store files that should be included
         should_include = []
+
+        # Check if file_dict is not None
         if file_dict is not None:
+            # Iterate over the file_dict and append the full file path to should_include list
             for i in range(0, len(file_dict)):
                 should_include.append(os.path.join(os.path.dirname(path), file_dict[i]))
 
+        # Construct the command for nuitka
         command = (
             f"python -m nuitka --clang --show-modules --follow-imports "
             f"--windows-company-name={companyname} --windows-product-version={product_version} "
             f"--output-dir={Output_dir_name} --verbose --assume-yes-for-downloads --onefile "
         )
+
+        # Append include-data-dir options to the command
         for i in range(0, len(should_include)):
             command += f"--include-data-dir={should_include[i]}={file_dict[i]} "
+
+        # Append enable-plugin options to the command
         for i in range(0, len(plugin_dict)):
             command += f"--enable-plugin={plugin_dict[i]} "
+
+        # Check if include_package_dict is not None
         if include_package_dict is not None:
+            # Append include-package options to the command
             for i in range(0, len(include_package_dict)):
                 command += f"--include-package={include_package_dict[i]} "
 
+        # Check if withconsole is True
         if withconsole:
+            # Check if icon is None
             if icon is None:
                 command = command + f"{buildfile_name}"
             else:
@@ -73,6 +90,7 @@ def build(
                     command + f"--windows-icon-from-ico={icon} " + f"{buildfile_name}"
                 )
         else:
+            # Check if icon is None
             if icon is None:
                 command = command + f"--windows-disable-console " + f"{buildfile_name}"
             else:
@@ -82,11 +100,13 @@ def build(
                     + f"--windows-icon-from-ico={icon} "
                 ) + f"{buildfile_name}"
 
+        # Measure the time taken for the build process
         start = time.time()
         subprocess.run(command, shell=True, check=True)
         end = time.time()
 
-        print(f"{end - start}s 사용됨")
+        # Print the time taken and the command used
+        print(f"{end - start}s elapsed")
         print(command)
     except Exception as e:
         print(e)
