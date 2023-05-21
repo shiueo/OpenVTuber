f284a4149058d0ee0b21e208488e769cb26bfb71
shiüo <shiueo.csh@gmail.com>
2023-05-21 17:39:19 +0900

    d

--- tools/prettier.py
+++ tools/prettier.py
@@ -25,15 +25,19 @@ DEALINGS IN THE SOFTWARE.
 import os
 import subprocess
 
-
 def code_format(path):
     try:
+        # Create a list to store target files for code formatting
         target_files = []
+
+        # Traverse the directory tree rooted at path
         for root, dirs, files in os.walk(path):
             for file in files:
+                # Check if the file is a Python file and not inside a "venv" directory
                 if file.endswith(".py") and root.find("venv") == -1:
                     target_files.append(os.path.join(root, file))
 
+        # Iterate over the target files and apply code formatting using Black
         for file in target_files:
             command = f"black {file}"
             print(command)
